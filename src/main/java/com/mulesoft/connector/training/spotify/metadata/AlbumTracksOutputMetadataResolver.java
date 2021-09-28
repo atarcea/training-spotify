7c058fd92442ebbb79a1c7aa01d1d39ad5032ff5
Adela Tarcea <atarcea@mulesoft.com>
2021-09-28 14:21:25 +0300

    - added static output metadata for albumTracks operation using ObjectTypeBuilder

--- src/main/java/com/mulesoft/connector/training/spotify/metadata/AlbumTracksOutputMetadataResolver.java
+++ src/main/java/com/mulesoft/connector/training/spotify/metadata/AlbumTracksOutputMetadataResolver.java
@@ -1,14 +1,12 @@
 package com.mulesoft.connector.training.spotify.metadata;
 
-import com.mulesoft.connector.training.spotify.metadata.type.Tracks;
+import org.mule.metadata.api.builder.ObjectTypeBuilder;
+import org.mule.metadata.api.builder.StringTypeBuilder;
 import org.mule.metadata.api.model.MetadataType;
-import org.mule.runtime.api.connection.ConnectionException;
 import org.mule.runtime.api.metadata.MetadataContext;
-import org.mule.runtime.api.metadata.MetadataResolvingException;
-import org.mule.runtime.api.metadata.resolving.AttributesTypeResolver;
 import org.mule.runtime.api.metadata.resolving.OutputTypeResolver;
 
-public class AlbumTracksOutputMetadataResolver implements OutputTypeResolver<Object>, AttributesTypeResolver<Object> {
+public class AlbumTracksOutputMetadataResolver implements OutputTypeResolver<Object>{
 
     @Override
     public String getResolverName() {
@@ -17,7 +15,50 @@ public class AlbumTracksOutputMetadataResolver implements OutputTypeResolver<Obj
 
     @Override
     public MetadataType getOutputType(MetadataContext context, Object key) {
-        return context.getTypeLoader().load(Tracks.class);
+        final ObjectTypeBuilder topObject = context.getTypeBuilder().objectType().id("album tracks");
+        topObject.addField().key("artists").value().arrayType().of(getInnerObject("artists", context));
+        topObject.addField().key("available_markets").value().arrayType().of(getInnerObject("available_markets", context));
+        topObject.addField().key("disc_number").value().numberType();
+        topObject.addField().key("duration_ms").value().numberType();
+        topObject.addField().key("explicit").value().booleanType();
+        topObject.addField().key("externalUrl").value().arrayType().of(getInnerObject("externalUrl", context));
+        topObject.addField().key("href").value().stringType();
+        topObject.addField().key("id").value().numberType();
+        topObject.addField().key("is_local").value().booleanType();
+        topObject.addField().key("name").value().stringType();
+        topObject.addField().key("preview_url").value().stringType();
+        topObject.addField().key("track_number").value().numberType();
+        topObject.addField().key("type").value().stringType();
+        topObject.addField().key("uri").value().stringType();
+
+        return topObject.build();
+    }
+
+    private MetadataType getInnerObject(String mainObjectName, MetadataContext context) {
+        final ObjectTypeBuilder objectTypeBuilder = context.getTypeBuilder().objectType().id(mainObjectName);
+
+        switch (mainObjectName) {
+
+            case "artists":
+                objectTypeBuilder.addField().key("externalUrl").value().arrayType().of(getInnerObject("externalUrl", context));
+                objectTypeBuilder.addField().key("href").value().stringType();
+                objectTypeBuilder.addField().key("id").value().numberType();
+                objectTypeBuilder.addField().key("name").value().stringType();
+                objectTypeBuilder.addField().key("type").value().stringType();
+                objectTypeBuilder.addField().key("uri").value().stringType();
+                return objectTypeBuilder.build();
+
+            case "externalUrl":
+                objectTypeBuilder.addField().key("spotify").value().stringType();
+                return objectTypeBuilder.build();
+
+            case "available_markets":
+                final StringTypeBuilder stringTypeBuilder = context.getTypeBuilder().stringType();
+                return stringTypeBuilder.build();
+
+            default: return null;
+        }
+
     }
 
     @Override
@@ -25,8 +66,4 @@ public class AlbumTracksOutputMetadataResolver implements OutputTypeResolver<Obj
         return "AlbumTracksOutputMetadataResolver";
     }
 
-    @Override
-    public MetadataType getAttributesType(MetadataContext context, Object key) throws MetadataResolvingException, ConnectionException {
-         return context.getTypeLoader().load(Tracks.class);
-    }
 }
